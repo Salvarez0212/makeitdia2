0c36939dbded43e14d40cde64f70ec9b32c3c5c6
Salvarez0212 <alvarezsebastian31@gmail.com>
2023-02-02 16:05:11 -0500

    Arreglo de errores

---
+++ main.js
@@ -0,0 +1,15 @@
+/*
+Escribe una función que tome dos arreglos (a y b) como argumentos.
+Crear un objeto que tenga propiedades con claves 'a' y valores correspondientes 'b'. Devolver el objeto.
+*/
+function myFunction(a, b){
+    let newArr = [];
+    for(let i = 0; i < a.length; i++){
+        newArr.push([a[i], b[i]])      
+    } 
+    console.log(Object.fromEntries(newArr));
+}
+
+myFunction(['a','b','c'],[1,2,3]);
+myFunction(['a','b','c'],[1,() => {}, {name: 'khriztian'}])
+myFunction(['name','hobbies','isAdmin'],['khriztian',['music', 'tv series'], false])
